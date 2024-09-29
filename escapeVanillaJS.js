7ed394702bf947266aa8cc611fa383007ad47501
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-29 09:25:53 +0200

    Edited asynchronous function and incorrect method

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -24,14 +24,15 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch('directions.json') 
             .then(response => response.json())
             .then(directions => {
-                navigateLabyrinth(directions)
+                return navigateLabyrinth(directions)
+            })
                     .then(message => {
                         // 🪲 Bug: Incorrect method
-                        document.getElementById("room3Result").innerHTML = message;
+                        const room3Results = document.getElementById("room3Result").textContent;
+                        message = room3Results;
                     });
             });
     });
-});
 
 function findMostRecentBook(books) {
     // 🪲 Bug: Logic error
